8eecd16c6002edfea7659e81f76541dc372b582a
Starco3000 <minhnguyen9686q6@gmail.com>
2025-01-18 10:16:36 +0700

    Update url backend in apiRequest

--- real-estate-frontend/client/src/services/apiRequest.js
+++ real-estate-frontend/client/src/services/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const URL = `${import.meta.env.VITE_BACKEND_URL}`;
-const URL = "https://real-estate-app-backend-hj1p.onrender.com"
+const URL = "https://real-estate-app-backend-oqfh.onrender.com"
 
 const apiRequest = axios.create({
   baseURL: URL,
